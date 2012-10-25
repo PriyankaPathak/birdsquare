0f4ce0c9b30d0c558e527ecd7c51271bedbccae3
NavaL Andrian <immotep@gmail.com>
2012-10-25 11:19:48 +0200

    Author: Naz/Naval added dependencies in gradle.build for testing views.

--- test/java/birdsquare/DummyControllerTest.java
+++ test/java/birdsquare/DummyControllerTest.java
@@ -1,23 +1,17 @@
+package birdsquare;
+
 import main.java.birdsquare.DummyController;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.ui.Model;
-import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 import javax.inject.Inject;
-import java.util.Collection;
-import java.util.Map;
 
-import static junit.framework.Assert.assertEquals;
-import static org.springframework.test.web.ModelAndViewAssert.*;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
 
 public class DummyControllerTest {
 
@@ -55,5 +49,6 @@ public class DummyControllerTest {
 
         final ModelAndView mav = handlerAdapter.handle(request, response, controller);
         assertViewName(mav, "dummypage");
+
     }
 }
