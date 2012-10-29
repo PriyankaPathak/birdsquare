60f2db703bbae24154beeb0101e088d600681a28
NavaL Andrian <immotep@gmail.com>
2012-10-29 06:47:59 +0200

    <Naval/Priyanka> #130: changed location to <textarea>

--- src/main/webapp/js/location.js
+++ src/main/webapp/js/location.js
@@ -1,21 +1,22 @@
 function getLocation(){
     if(navigator.geolocation)
-        var pos = navigator.geolocation.getCurrentPosition(showPosition, errorGeoSupport);
+        navigator.geolocation.watchPosition(showPosition, showError);
     else
-        alert("your browser does not support geolocation");
+        alert("your browser does not support Geolocation");
 }
 
 
 function showPosition(position){
+    alert("in here");
+
     var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({'latLng': latlng}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             if (results[1]) {
-                document.getElementById("location").innerHTML="<input value=\" "  +
-                    results[1].formatted_address +
-                    "\" size=\"35\" type =\"text\" name=\"location\"  >";
+                document.getElementById("location").innerHTML="<textarea rows=\"2\" "+
+                "cols=\"30\" name=\"location\">" + results[1].formatted_address + "</textarea>";
             }
         } else {
             alert("Geocoder failed due to: " + status);
@@ -26,6 +27,26 @@ function showPosition(position){
         }
     });
 }
+
+function showError(error)
+{
+    switch(error.code)
+    {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
+
 
 function errorGeoSupport(){
     alert("GooGle map failure: No Internet Connection. Please, enter location manually");
