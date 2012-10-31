0dbb2af6378ce9d941355373fc441253aa7fc22e
NavaL Andrian <immotep@gmail.com>
2012-10-31 04:25:59 +0200

    <Naval> #180: removed redundant deploy_to_... scripts -- after configuring go server.

--- scripts/deploy_to_qa.sh
+++
@@ -1,1 +0,0 @@
-scp -r artifacts/BirdSquare-1.0.war root@qa.birdsquare.in:/usr/local/jetty/webapps/
--- scripts/deploy_to_showcase.sh
+++
@@ -1,1 +0,0 @@
-scp -r artifacts/BirdSquare-1.0.war root@showcase.birdsquare.in:/usr/local/jetty/webapps/
