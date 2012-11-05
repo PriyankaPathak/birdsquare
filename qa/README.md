5cb68f6e1d86279774a4b9002b7852508a8d8e06
Alok Simha <lukasimha@gmail.com>
2012-11-05 15:58:09 +0530

    <Sneha/Luka/Ritika> #133: Moved qa code to functional package under test. Wrote gradle script to start and stop jetty before and after execution of test code

--- qa/README.md
+++
@@ -1,6 +0,0 @@
-# QA Code Lives Here
-
-Hi Developers, nice to meet you. We're going to put some test code here. I hope
-you like it. It's going to make this application awesomesauce!
-
-Ok, time to do a tabata, be right back.
--- src/test/java/birdsquare/functional/HomePageTest.java
+++ src/test/java/birdsquare/functional/HomePageTest.java
@@ -1,13 +1,14 @@
+package birdsquare.functional;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 public class HomePageTest {
 
