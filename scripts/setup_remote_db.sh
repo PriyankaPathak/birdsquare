7c7602b438fae5e852db80851c052b0d6f74e1b4
NazneenRupawalla <nazneenr@thoughtworks.com>
2012-11-09 15:00:15 +0530

    <BirdSquare team> #NaN: Populating bird data remotely (Hope it works !)

--- scripts/setup_remote_db.sh
+++ scripts/setup_remote_db.sh
@@ -1,4 +1,8 @@
+scp database/data.csv root@$1.birdsquare.in:/tmp
+scp database/populate_data.sql root@$1.birdsquare.in:/tmp
+
 ssh -t root@$1.birdsquare.in mysql -uroot < "database/create_schema.sql"
+ssh -t root@$1.birdsquare.in 'cd /tmp && mysql -uroot --local-infile < "populate_data.sql"'
 
 # copy populate sql script to remote server
 # run mysql and use that populate script that was copied to the server
